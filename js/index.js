603a3c358a878b0b480e04a8d9f706d667fd9b26
Lego Stax <legostax@gmail.com>
2018-08-22 11:50:42 -0700

    Basic fullPage setup w/ homepage improvement.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+new fullpage("#fullpage", {
+	licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
+	fixedElements: "#nav-bar"
+});
+
+// fullpage_api.setAllowScrolling(false);
+
+console.log("everything should have ran")
