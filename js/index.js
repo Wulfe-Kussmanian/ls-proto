55ea4cf757cbfd512fdfd94f81a6a2c3404d2d3e
legostax <legostax@gmail.com>
2018-08-24 21:38:26 -0700

    Added a plethora of stuffs

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 new fullpage("#fullpage", {
 	licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
-	fixedElements: "#nav-bar"
+	fixedElements: "#nav-bar",
+	onLeave: function(origin, destination, direction){
+		if(destination.index == 0){
+			console.log("make nav-bar transparent");
+			document.getElementById("nav-bar").style.backgroundColor = null;
+		}else{
+			console.log("make it a black");
+			document.getElementById("nav-bar").style.backgroundColor = "#000";
+		}
+	}
 });
 
 // fullpage_api.setAllowScrolling(false);
